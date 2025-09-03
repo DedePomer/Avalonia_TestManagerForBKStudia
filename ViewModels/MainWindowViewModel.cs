56fd0b647f97542913536016036402c2e7578f78
t343s <t343s@PC-01>
2025-09-03 19:41:41 +0300

    Добавьте файлы проекта.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Avalonia_TestManagerForBKStudia.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        private int _index = 0;
+        
+        [ObservableProperty]
+        private string _greeting = "Welcome to Avalonia!";
+
+
+        public MainWindowViewModel()
+        {
+        }
+
+        [RelayCommand]
+        private void ChangeText(object? obj)
+        {
+            Greeting = Convert.ToString(_index);
+            _index++;
+        }
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Avalonia_TestManagerForBKStudia.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+
+}
