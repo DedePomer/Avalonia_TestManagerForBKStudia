290466bdccdedb93087b2710a657fee1d813da4c
t343s <t343s@PC-01>
2025-09-03 19:50:10 +0300

    добавил пакетики

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
@@ -8,7 +7,7 @@ namespace Avalonia_TestManagerForBKStudia.ViewModels
     public partial class MainWindowViewModel : ViewModelBase
     {
         private int _index = 0;
-        
+
         [ObservableProperty]
         private string _greeting = "Welcome to Avalonia!";
 
@@ -20,6 +19,7 @@ namespace Avalonia_TestManagerForBKStudia.ViewModels
         [RelayCommand]
         private void ChangeText(object? obj)
         {
+            var a = obj;
             Greeting = Convert.ToString(_index);
             _index++;
         }
