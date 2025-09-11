a8ac953e1ae806dd3cfbe9bf2c3cd800987ec51e
t343s <t343s@PC-01>
2025-09-11 04:43:56 +0300

    веслюсь с хостингом

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,11 +1,11 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
-using System.Linq;
 using Avalonia.Markup.Xaml;
+using Avalonia_TestManagerForBKStudia.Infrastructure.Extensions;
 using Avalonia_TestManagerForBKStudia.ViewModels;
 using Avalonia_TestManagerForBKStudia.Views;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Avalonia_TestManagerForBKStudia;
 
@@ -18,30 +18,29 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        BindingPlugins.DataValidators.RemoveAt(0);
+
+        
+
+        IServiceCollection collection = new ServiceCollection();
+        collection.BuildProvider();
+
+        var services = collection.BuildServiceProvider();
+
+        var vm = services.GetRequiredService<MainWindowViewModel>();
+
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
-            // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
-            DisableAvaloniaDataAnnotationValidation();
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainWindowViewModel(),
+                DataContext = vm
             };
         }
 
         base.OnFrameworkInitializationCompleted();
     }
 
-    private void DisableAvaloniaDataAnnotationValidation()
-    {
-        // Get an array of plugins to remove
-        var dataValidationPluginsToRemove =
-            BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
 
-        // remove each entry found
-        foreach (var plugin in dataValidationPluginsToRemove)
-        {
-            BindingPlugins.DataValidators.Remove(plugin);
-        }
-    }
 }
+
+
---
+++ Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia_TestManagerForBKStudia.Views;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Avalonia_TestManagerForBKStudia.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void BuildProvider(this IServiceCollection collection)
+        {
+            collection.AddSingleton<MainWindow>();
+        }
+    }
+}
