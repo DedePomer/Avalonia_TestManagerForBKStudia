5f7ded605e11f2c82ccdc6163520b1a19f8e56fc
t343s <t343s@PC-01>
2025-09-12 17:59:55 +0300

    добавил селектор и темплайт

---
+++ Infrastructure/Selectors/CreateTestTemplateSelctor.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Avalonia.Markup.Xaml.Templates;
+using Avalonia_TestManagerForBKStudia.Models.UserDataTypes;
+
+namespace Avalonia_TestManagerForBKStudia.Infrastructure.Selectors
+{
+    public class CreateTestTemplateSelctor : IDataTemplate
+    {
+        public required DataTemplate QuestionWithTextAnswerTemplate { get; set; }
+        public required DataTemplate MultipleChoiceQuestionWithOneAnswerTemplate { get; set; }
+
+        public Control? Build(object? param)
+        {
+            if (param is QuestionWithTextAnswer)
+            {
+                return QuestionWithTextAnswerTemplate.Build(param);
+            }
+            else if (param is MultipleChoiceQuestionWithOneAnswer)
+            {
+                return MultipleChoiceQuestionWithOneAnswerTemplate.Build(param);
+            }
+
+            return null;
+        }
+
+        public bool Match(object? data)
+        {
+            return data is QuestionWithTextAnswer || data is MultipleChoiceQuestionWithOneAnswer;
+        }
+    }
+}
