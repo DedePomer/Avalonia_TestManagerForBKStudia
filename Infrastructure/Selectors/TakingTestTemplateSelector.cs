56d8fbcddb8f846236ebb594cc3aa0042de91d5c
t343s <t343s@PC-01>
2025-09-14 19:05:56 +0300

    добавил новый селектор

---
+++ Infrastructure/Selectors/TakingTestTemplateSelector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml.Templates;
+using Avalonia_TestManagerForBKStudia.Models.UserDataTypes;
+
+namespace Avalonia_TestManagerForBKStudia.Infrastructure.Selectors
+{
+    public class TakingTestTemplateSelector
+    {
+
+
+        public DataTemplate QuestionWithTextAnswerTemplate { get; set; }
+        public DataTemplate MultipleChoiceQuestionWithOneAnswerTemplate { get; set; }
+
+        public Control? Build(object? param)
+        {
+            if (param is QuestionWithTextAnswer)
+            {
+                return QuestionWithTextAnswerTemplate.Build(param);
+            }
+            else if (param is MultipleChoiceQuestionWithOneAnswer)
+            {
+                return MultipleChoiceQuestionWithOneAnswerTemplate.Build(param);
+            }
+
+            return null;
+        }
+
+        public bool Match(object? data)
+        {
+            return data is QuestionWithTextAnswer || data is MultipleChoiceQuestionWithOneAnswer;
+        }
+    }
+}
